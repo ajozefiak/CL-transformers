2de0857f79540fbfed81c76d8e85604da455b199
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-13 04:09:25 -0400

    Fix typo

--- src/experiments/PS.py
+++ src/experiments/PS.py
@@ -162,7 +162,7 @@ def run_experiment_PS(text, B, T, N, epochs, tasks, seed, save_neuron_ages, save
 
     # Initialize the random key
     key = jr.PRNGKey(seed)
-    key, split_key = key.split(key)
+    key, split_key = jr.split(key)
 
     # TODO: change this if we want to play with the ModelConfig - maybe make it an argument
     config = ModelConfig()
