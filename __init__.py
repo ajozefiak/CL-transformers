4f40beb860ff4c2c9f603db92e0cca7121ed1528
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-14 02:12:11 -0400

    revert __init__.py file

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # Models
 from .src.models import *
 
-from .src.experiment import *
-# from .src.experiments.PS import run_experiment_PS
-# from .src.experiments.PS_factory import run_experiment_PS_factory
+# from .src.experiments import *
+from .src.experiments.PS import run_experiment_PS
+from .src.experiments.PS_factory import run_experiment_PS_factory
 
 # # Import everything in src/__init__.py __all__
 # from .src import *
