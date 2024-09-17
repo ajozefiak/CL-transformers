e59891c23d67e68acde25906ed3d0fa693ab81ac
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-16 21:37:39 -0400

    update __init__.py file

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from .src.models import *
 from .src.experiments.PS import run_experiment_PS
 from .src.experiments.PS_factory import run_experiment_PS_factory
 from .src.experiments.PS_factory_test_reset import run_experiment_PS_factory_test_reset
+from .src.experiments.Alt_CATN import run_experiment_Alt_CATN
 
 # # Import everything in src/__init__.py __all__
 # from .src import *
