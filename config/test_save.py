fb8f8b3704c90bacf6b011852a200dfa4402731c
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-22 15:49:09 -0400

    add temp config files to test save

---
+++ config/test_save.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pickle
+
+X = np.ones((1000,1000))
+path = "/nobackup1/jozefiak/CL/Results/PS/Test_Dir/"
+
+ with open(path + 'X.pkl', 'wb') as f:
+    pickle.dump(neuron_ages_array, f)
---
+++ config/test_save.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                     
+#SBATCH --job-name=test_save                                                  
+#SBATCH --output=out_test_save_%A_%a.txt  # %A is the job array ID, %a is the task ID
+#SBATCH --error=err_test_save_%A_%a.txt                                             
+#SBATCH -p sched_mit_sloan_batch_r8                                               
+#SBATCH --ntasks=1                                                              
+#SBATCH --cpus-per-task=1                                                    
+#SBATCH --mem-per-cpu=8GB                                                      
+#SBATCH --time=10:00                                                         
+#SBATCH --gres=gpu:0  
+#SBATCH --array=1                                                          
+
+source /etc/profile.d/modules.sh                                                
+module load sloan/python/3.11.4
+
+python3 test_save.py
