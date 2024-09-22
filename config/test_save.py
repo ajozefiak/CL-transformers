92dc02ce36c4cccca1a6e122390238e52ffd323f
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-22 15:56:53 -0400

    fix saving

--- config/test_save.py
+++ config/test_save.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pickle
+import os
 
 X = np.ones((1000,1000))
 path = "/nobackup1/jozefiak/CL/Results/PS/Test_Dir/"
+
+if not os.path.exists(path):
+   os.makedirs(save_path)
 
 with open(path + 'X.pkl', 'wb') as f:
    pickle.dump(neuron_ages_array, f)
