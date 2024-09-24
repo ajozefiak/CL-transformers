00ca24c86deba49a1207270a00caca889a695fc5
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-24 02:00:16 -0400

    fix imports

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from .src.experiments.PS import run_experiment_PS
 from .src.experiments.PS_factory import run_experiment_PS_factory
 from .src.experiments.PS_factory_test_reset import run_experiment_PS_factory_test_reset
 from .src.experiments.Alt_CATN import run_experiment_Alt_CATN
-from .src.experiments.ALT import run_experiment_ATN
+from .src.experiments.ATN import run_experiment_ATN
 
 # # Import everything in src/__init__.py __all__
 # from .src import *
--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -1,4 +1,4 @@
 from .PS import run_experiment_PS
 from .PS_factory import run_experiment_PS_factory
 from .Alt_CATN import run_experiment_Alt_CATN
-from .ALT import run_experiment_ATN
+from .ATN import run_experiment_ATN
