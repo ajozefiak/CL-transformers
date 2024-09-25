078a1db1d47da65efbdb258f0b1fa1b49bc32a25
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-09-25 03:21:26 -0400

    fupload config files

---
+++ config/092524_final_PS_experiments/PS_092524_256_neurons_500_tasks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                     
+#SBATCH --job-name=PS_092524                                                    
+#SBATCH --output=out_PS_092524_%A_%a.txt  # %A is the job array ID, %a is the task ID
+#SBATCH --error=err_PS_092524_%A_%a.txt                                             
+#SBATCH -p sched_mit_sloan_gpu_r8                                               
+#SBATCH --ntasks=1                                                              
+#SBATCH --cpus-per-task=2                                                       
+#SBATCH --mem-per-cpu=32GB                                                      
+#SBATCH --time=24:00:00                                                         
+#SBATCH --gres=gpu:1  
+#SBATCH --array=10-25                                                         
+
+source /etc/profile.d/modules.sh                                                
+module load sloan/python/3.11.4
+
+python3 PS_092524_256_neurons_500_tasks.py $SLURM_ARRAY_TASK_ID
