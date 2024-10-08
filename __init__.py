ab2af066f6bdbdddd08984ac275ffd64066a541b
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-10-08 03:52:54 -0400

    add expeirments and config files for correctly suffling data

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from .src.experiments.PS_factory import run_experiment_PS_factory
 from .src.experiments.PS_factory_test_reset import run_experiment_PS_factory_test_reset
 from .src.experiments.Alt_CATN import run_experiment_Alt_CATN
 from .src.experiments.ATN import run_experiment_ATN
+from .src.experiments.PS_factory_100724 import run_experiment_PS_100724
 
 # # Import everything in src/__init__.py __all__
 # from .src import *
--- src/experiments/__init__.py
+++ src/experiments/__init__.py
@@ -2,3 +2,4 @@ from .PS import run_experiment_PS
 from .PS_factory import run_experiment_PS_factory
 from .Alt_CATN import run_experiment_Alt_CATN
 from .ATN import run_experiment_ATN
+from .PS_factory_100724 import run_experiment_PS_100724
