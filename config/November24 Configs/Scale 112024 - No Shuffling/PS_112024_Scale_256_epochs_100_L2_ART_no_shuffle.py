8047c42315ef654acae736e835f903712cfa7ec5
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-11-20 04:52:45 -0500

    scale reg_str and lr according to scaling

--- config/November24 Configs/Scale 112024 - No Shuffling/PS_112024_Scale_256_epochs_100_L2_ART_no_shuffle.py
+++ config/November24 Configs/Scale 112024 - No Shuffling/PS_112024_Scale_256_epochs_100_L2_ART_no_shuffle.py
@@ -57,13 +57,16 @@ save_weights_freq = int(epochs*batches)
 # save_weights_freq = int((B * batches) / (T-1))
 
 
-reg_strs = [1e-4]
+reg_strs = [1e-4 / width_factor]
+lr = 1e-3 / width_factor
+
 # for alg in ['L2', 'ART-L2', 'Vanilla', 'L2Init', 'ART']:
 for alg in ['ART-L2']:
   for reg_str in reg_strs:
     alg_params = {'threshold': 16,
                     'reset_percentile': 0.95,
                     'reset_freq': (1e-4 * (epochs / 20)),
-                    'reg_str': reg_str}  
+                    'reg_str': reg_str,
+                    'lr': lr}  
     save_path = save_path_root + f'/epochs_{epochs}/' + alg + '_' + str(reg_str) + '/'
     res = CL_transformers.run_experiment_PS_112024_no_shuffle(config, alg, alg_params, text, B, T, N, epochs, tasks, seed, save_neuron_ages, save_results, save_path, verbose, print_freq, save_weights, save_weights_freq)
