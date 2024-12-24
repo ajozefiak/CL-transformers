e02caee98f8e3c80d6fc6dad6164237a21b5512e
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2024-12-24 01:45:30 -0800

    add experiment

--- config/December24 Configs/ViT/lr_sweep/lr_sweep.py
+++ config/December24 Configs/ViT/lr_sweep/lr_sweep.py
@@ -1,21 +1,30 @@
-# config:
-num_layers = 3
-hidden_dim = int(num_layers * 16)
-lr = 1e-3
-print(f"Number of layers: {num_layers}")
-print(f"Hidden dimension: {hidden_dim}")
-print(f"Learning rate: {lr}")
+import sys
+import math
+import CL_transformers
 
-config = CL_transformers.ModelConfigViT(num_layers=num_layers, hidden_dim=hidden_dim, lr=lr, num_classes=2)
-alg = 'Vanilla'
-alg_params = {}
-seed = 0
+# Read arguments
+seed = int(sys.argv[1])
+layers = int(sys.argv[2])
 
-cluster = True
-# Use default experiment config
-experiment_config = {}
+learning_rates = [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6]
 
-# TODO:
-save_path = '/content/drive/MyDrive/CL LLM/Test ViT/Test_pkg/Vanilla/seed_0/'
+# Run the experiment for each learning rate
+for lr in learning_rates:
+    
+    # config:
+    num_layers = layers
+    hidden_dim = int(num_layers * 16)
+    config = CL_transformers.ModelConfigViT(num_layers=num_layers, hidden_dim=hidden_dim, lr=lr, num_classes=2)
 
-res = CL_transformers.run_CI_ViT_R1_experiment(config, alg, alg_params, seed, save_path, cluster)
+    alg = 'Vanilla'
+    # No alg_params necessary for Vanilla algorithm
+    alg_params = {}
+
+    cluster = True
+    # Use default experiment config, so we can leave this blank
+    experiment_config = {}
+
+    # TODO:
+    save_path = f'/pool001/jozefiak/CI_ViT/lr_sweep/{alg}_L{num_layers}_lr_{lr}/seed_{seed}/'
+
+    res = CL_transformers.run_CI_ViT_R1_experiment(config, alg, alg_params, seed, save_path, cluster, experimental_config)
