f3e480cf0b4c040f508f68034381ef84f14b6749
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-01-01 02:51:29 -0800

    update configs

--- config/December24 Configs/ViT/opt_h_param_sweep/L2_log.py
+++ config/December24 Configs/ViT/opt_h_param_sweep/L2_log.py
@@ -7,21 +7,23 @@ seed = int(sys.argv[1])
 layers = int(sys.argv[2])
 lr = float(sys.argv[3])
 
+reg_str = TODO
+    
 # config:
 num_layers = layers
 hidden_dim = int(num_layers * 16)
 n_neurons = int(4 * hidden_dim)
 config = CL_transformers.ModelConfigViT(num_layers=num_layers, hidden_dim=hidden_dim, n_neurons=n_neurons, lr=lr, num_classes=2)
 
-alg = 'Vanilla'
+alg = 'L2'
 # No alg_params necessary for Vanilla algorithm
-alg_params = {}
+alg_params = {"reg_str": reg_str}
 
 cluster = True
 # Use default experiment config, so we can leave this blank
 experiment_config = {}
 
 # TODO:
-save_path = f'/pool001/jozefiak/CI_ViT/opt_h_param_sweep/{alg}_L{num_layers}_lr_{lr}/seed_{seed}/'
+save_path = f'/pool001/jozefiak/CI_ViT/opt_h_param_sweep/{alg}_L{num_layers}_lr_{lr}_reg_str_{reg_str}/seed_{seed}/'
 
 res = CL_transformers.run_CI_ViT_R1_log_correlates(config, alg, alg_params, seed, save_path, cluster, experiment_config)
---
+++ config/December24 Configs/ViT/opt_h_param_sweep/L2_log.sh
@@ -0,0 +1,22 @@
+#!/bin/bash                                                                     
+#SBATCH --job-name=CI_lr_sweep                                                    
+#SBATCH --output=out_CI_lr_sweep_%A_%a.txt  # %A is the job array ID, %a is the task ID
+#SBATCH --error=err_CI_lr_sweep_%A_%a.txt                                             
+#SBATCH -p sched_mit_sloan_gpu_r8                                               
+#SBATCH --ntasks=1         
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64GB
+#SBATCH --time=24:00:00                                                         
+#SBATCH --gres=gpu:1  
+#SBATCH --array=5-9      
+
+source /etc/profile.d/modules.sh                                                
+module load sloan/python/3.11.4
+
+# Accept scale and epochs as command-line arguments
+layers=$1
+lr=$2
+
+echo "Scale: $layers, lr: $lr"
+
+python3 L2_log.py $SLURM_ARRAY_TASK_ID $layers $lr
