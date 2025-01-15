9bc15855fb46890e503d53996cfcc1487f567ebb
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-01-15 10:13:36 -0800

    add vanilla config

---
+++ config/December24 Configs/ViT/logging_011525/Vanilla_log.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import CL_transformers
+
+# Read arguments
+seed = int(sys.argv[1])
+layers = int(sys.argv[2])
+lr = float(sys.argv[3])
+
+# config:
+num_layers = layers
+hidden_dim = int(num_layers * 16)
+n_neurons = int(4 * hidden_dim)
+config = CL_transformers.ModelConfigViT(num_layers=num_layers, hidden_dim=hidden_dim, n_neurons=n_neurons, lr=lr, num_classes=2)
+
+alg = 'Vanilla'
+# No alg_params necessary for Vanilla algorithm
+alg_params = {}
+
+cluster = True
+# Use default experiment config, so we can leave this blank
+experiment_config = {}
+
+# TODO:
+save_path = f'/nobackup1/jozefiak/CI_ViT/opt_h_param_sweep/{alg}_L{num_layers}_lr_{lr}/seed_{seed}/'
+
+res = CL_transformers.run_CI_ViT_R1_log_correlates_2(config, alg, alg_params, seed, save_path, cluster, experiment_config)
