e94ab28ff519ef9118bf84128a897d39288aed36
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-01-15 08:39:59 -0800

    add new experiment file

--- __init__.py
+++ __init__.py
@@ -18,6 +18,7 @@ from .src.experiments.PS_factory_112024_cold_start import run_experiment_PS_1120
 from .src.experiments.CI_ViT_V1 import run_CI_ViT_R1_experiment
 from .src.experiments.CI_ViT_V1_resets import run_CI_ViT_R1_reset_experiment
 from .src.experiments.CI_ViT_V1_log_correlates import run_CI_ViT_R1_log_correlates
+from .src.experiments.CI_ViT_V1_log_correlates_2 import run_CI_ViT_R1_log_correlates_2
 
 # # Import everything in src/__init__.py __all__
 # from .src import *
