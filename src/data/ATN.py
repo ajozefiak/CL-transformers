45d60366ff21fe699feca581e3d1303ddd0efd4f
ajozefiak <adam.jozefiak@alumni.ubc.ca>
2025-01-20 15:10:09 -0800

    fix loading of path

--- src/data/ATN.py
+++ src/data/ATN.py
@@ -5,7 +5,7 @@ import jax.numpy as jnp
 import pickle
 
 def get_ATN_data(path):
-    path = path = "/ds_tokenized_512.pkl"
+    path = path + "/ds_tokenized_512.pkl"
     with open(path, 'rb') as f:
         data = pickle.load(f)
     return data
